d232e3d63106f2039508ab22d3ecbb7d3127e624
Narvatov <hipi96222@gmail.com>
2024-06-02 16:48:10 +0300

    Initial commit

---
+++ src/main/java/com/narvatov/news_service_registry/NewsServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.narvatov.news_service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NewsServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narvatov/news_service_registry/NewsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narvatov.news_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
